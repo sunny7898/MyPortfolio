b55f4ad12f82ae7e33bff82806558509db1f8f12
Sunny Goswami <sungoswami07gmail.com>
2024-10-31 00:32:54 +0530

    Projects page done

--- src/components/ProjectsComponent/ProjectCard.jsx
+++ src/components/ProjectsComponent/ProjectCard.jsx
@@ -1,30 +1,33 @@
-// src/components/ProjectCard.js
 import React from "react";
-import "./ProjectCard.css"; // Ensure to create the CSS file
+import "./ProjectCard.css";
 
 function ProjectCard({ image, title, summary, githubLink, demoLink }) {
   return (
     <div className="project-card">
-      <img src={image} alt={title} className="project-image" />
-      <h3 className="project-title">{title}</h3>
-      <p className="project-summary">{summary}</p>
-      <div className="project-links">
-        <a
-          href={githubLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="project-link"
-        >
-          GitHub
-        </a>
-        <a
-          href={demoLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="project-link"
-        >
-          Demo
-        </a>
+      <div className="project-image-container">
+        <img src={image} alt={title} className="project-image" />
+      </div>
+      <div className="project-text-container">
+        <h3 className="project-title">{title}</h3>
+        <p className="project-summary">{summary}</p>
+        <div className="project-links">
+          <a
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-link"
+          >
+            GitHub
+          </a>
+          <a
+            href={demoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-link"
+          >
+            Demo
+          </a>
+        </div>
       </div>
     </div>
   );
