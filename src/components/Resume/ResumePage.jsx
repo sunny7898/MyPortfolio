6fa08f95291681783f3949ab8f2dd096db2e41c7
Sunny Goswami <sungoswami07gmail.com>
2024-10-31 09:28:55 +0530

    Resume layout fix

--- src/components/Resume/ResumePage.jsx
+++ src/components/Resume/ResumePage.jsx
@@ -7,35 +7,42 @@ import { FaDownload } from "react-icons/fa6";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-// Set the worker to the pdfjs-dist worker - use min.mjs in place min.js
+// Set the worker to the pdfjs-dist worker
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
 function ResumePage() {
   const [scale, setScale] = useState(1);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    function updateScale() {
-      if (window.innerWidth > 1440) {
+    function updateDimensions() {
+      const width = window.innerWidth;
+      setWindowWidth(width);
+
+      function updateScale() {
+        const pdfPageWidth = 612; // Width in pixels for a standard PDF page (US Letter)
+        const newScale = width / pdfPageWidth;
+        setScale(newScale);
+      }
+
+      if (width > 1440) {
         setScale(2);
-      } else if (window.innerWidth > 1024) {
-        setScale(1.35);
-      } else if (window.innerWidth > 769) {
-        setScale(1);
-      } else if (window.innerWidth > 481) {
-        setScale(0.65);
-      } else if (window.innerWidth > 320) {
-        setScale(0.5);
-      } else if (window.innerWidth > 280) {
-        setScale(0.25);
-      } else {
+      } else if (width > 1024) {
         setScale(1.5);
+      } else if (width > 768) {
+        setScale(1.1);
+      } else if (width > 480) {
+        updateScale();
+      } else {
+        setScale(0.6);
       }
     }
-    updateScale();
 
-    // Add event listener to update scale on resize
-    window.addEventListener("resize", updateScale);
-    return () => window.removeEventListener("resize", updateScale);
+    updateDimensions();
+
+    // Update scale on resize
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
   }, []);
 
   return (
@@ -46,7 +53,7 @@ function ResumePage() {
           <Page pageNumber={1} scale={scale} />
         </Document>
       </div>
-      <DownloadButton />
+      {windowWidth > 768 && <DownloadButton />}{" "}
     </div>
   );
 }
